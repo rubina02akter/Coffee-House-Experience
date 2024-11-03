3ca969a6213f427ac3dbaddee3b242ab9aeee967
rubina02akter <rubinaakter0108@gmail.com>
2024-11-03 14:12:17 +0600

    final

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@
 const Heading = ({title, subtitle}) => {
 
   return (
-    <div className='flex flex-col w-full justify-center items-center'>
+    <div className='flex flex-col w-full justify-center items-center py-12'>
       <h1 className='text-xl md:text-2xl lg:text-4xl font-thin mb-4'>
         {title}
       </h1>
